234f0e6e9f8b16c2de7f68029c5f3b40ebfe894a
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2023-01-05 14:13:22 +0000

    [pause]bookers2-debag5

--- rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
     redirect_to request.referer
-  end
+    end
 
   def destroy
     book = Book.find(params[:book_id])
